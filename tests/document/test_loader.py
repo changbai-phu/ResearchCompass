bd6c44273ba06962d71204509e9f1f723f499216
Peiqi Hu <hupeiqichina@gmail.com>
2026-07-24 10:59:16 +0800

    adding __init__.py for easy import

--- tests/document/test_loader.py
+++ tests/document/test_loader.py
@@ -1,1 +1,8 @@
 import pytest
+from src.modules.document.loader import DocumentLoader
+
+def test_loader():
+    loader = DocumentLoader("data/raw/sample_papers/adv_in_space_quan_comm.pdf")
+    loader_doc = loader.load()
+
+    assert len(loader_doc) > 0
