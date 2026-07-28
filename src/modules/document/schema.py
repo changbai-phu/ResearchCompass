6c2a40283599ac6587d2cd3ee740d646c8316f34
Peiqi Hu <hupeiqichina@gmail.com>
2026-07-28 12:30:23 +0800

    finishing chunker, adding Chunk dataclass

--- src/modules/document/schema.py
+++ src/modules/document/schema.py
@@ -4,4 +4,23 @@ from dataclasses import dataclass
 class Page:
     text: str
     page_number: int
-    #source_file: str
+    source: str #filename
+
+@dataclass
+class Chunk:
+    id: str
+    text: str
+    page_number: int
+    source: str
+    # embedding:
+    # metadata:
+    # score
+
+'''
+@dataclass
+class MetaData:
+    source: str
+    title: str
+    authors: list[str]
+    year: int
+'''
