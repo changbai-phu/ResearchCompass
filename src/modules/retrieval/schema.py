22eeb5ec3af07de9a5cdd7861011ed1a1627875a
Peiqi Hu <hupeiqichina@gmail.com>
2026-08-05 16:13:56 +0800

    finishing retriever v1

--- src/modules/retrieval/schema.py
+++ src/modules/retrieval/schema.py
@@ -1,9 +1,16 @@
 from dataclasses import dataclass
 from typing import List
 
+from src.modules.document.schema import Chunk
+
 @dataclass
 class Embeddings:  
     chunk_id: str
     model_name: str
     dimension: int
     vector: List[float]
+
+@dataclass
+class RetrievedChunk:
+    chunk: Chunk
+    score: float
