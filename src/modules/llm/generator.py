48685a5a54ea9f129e2dd96b644027f9de849aba
Peiqi Hu <hupeiqichina@gmail.com>
2026-08-11 15:26:34 +0800

    adding basic function structure placeholder

---
+++ src/modules/llm/generator.py
@@ -0,0 +1,12 @@
+'''
+Call LLM API with the prompt (from prompt_builder).
+Will use Qwen or Deepseek first, later change to OpenAI for comparison. 
+'''
+
+
+
+class LLMGenerator:
+    def __init__(self) -> None:
+
+
+    def generate(self, prompt: str) -> str:
