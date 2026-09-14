54b94b21cec91341667d23fb02a8f98fe9267591
Peiqi Hu <hupeiqichina@gmail.com>
2026-09-14 21:33:25 +0800

    update schema by adding LLMResponse dataclass

--- src/modules/generation/schema.py
+++ src/modules/generation/schema.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
+from typing import List
 
 @dataclass
 class Message:      # a single structured chat message payload object
     role: str       # e.g, system or user
     content: str    # actual textual message body content
+
+@dataclass
+class LLMResponse:
+    answer: str
+    sources: List[str]
